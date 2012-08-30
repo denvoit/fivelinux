d90a92f3135300df3a4d821a2e33e4d8e4a5bff7
antonio.fivetech <antonio.fivetech@gmail.com>
2012-08-30 09:32:08 +0000

    More on fiveform.prg

--- source/winapi/says.c
+++ source/winapi/says.c
@@ -1,18 +1,38 @@
 #include <hbapi.h>
 #include <gtk/gtk.h>
 
+gboolean button_press_event( GtkWidget * hWnd, GdkEventButton * event );
+gboolean motion_notify_event( GtkWidget * hWnd, GdkEventMotion * event );
+
 HB_FUNC( CREATESAY )
 {
    GtkWidget * hWnd = gtk_label_new( hb_parc( 1 ) );
+   GtkWidget * event_box = gtk_event_box_new();
 
-   hb_retnl( ( HB_ULONG ) hWnd );
+   gtk_signal_connect( GTK_OBJECT( event_box ), "button_press_event",
+                       ( GtkSignalFunc ) button_press_event, NULL );
+
+   gtk_signal_connect( GTK_OBJECT( event_box ), "motion_notify_event",
+                       ( GtkSignalFunc ) motion_notify_event, NULL );
+
+   gtk_widget_set_events( event_box, GDK_EXPOSURE_MASK
+			 | GDK_LEAVE_NOTIFY_MASK
+			 | GDK_BUTTON_PRESS_MASK
+			 | GDK_POINTER_MOTION_MASK
+			 | GDK_POINTER_MOTION_HINT_MASK );
+
+   gtk_container_add( GTK_CONTAINER( event_box ), hWnd );
+   gtk_widget_show( hWnd );
+
+   hb_retnl( ( HB_ULONG ) event_box );
 }
 
 HB_FUNC( SAYSETTEXT )
 {
    GtkWidget * hWnd = ( GtkWidget * ) hb_parnl( 1 );
+   GList * children = gtk_container_get_children( ( GtkContainer * ) hWnd );
 
-   gtk_label_set_text( ( GtkLabel * ) hWnd, hb_parc( 2 ) );
+   gtk_label_set_text( children->data, hb_parc( 2 ) );
 }
 
 HB_FUNC( SAYGETTEXT )
