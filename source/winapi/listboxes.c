c1bc418bb9e9d9a7bf54b276e643998029a16569
antonio.fivetech <antonio.fivetech@gmail.com>
2012-09-01 10:18:24 +0000

    More on fiveform.prg

--- source/winapi/listboxes.c
+++ source/winapi/listboxes.c
@@ -12,6 +12,9 @@ void LbxChangeEvent( GtkList * hWnd, GtkWidget * hItem, gpointer user_data );
 gint LostFocusEvent( GtkWidget * hWnd, GdkEventFocus * event,
                      gpointer user_data );
 
+gboolean button_press_event( GtkWidget * hWnd, GdkEventButton * event );
+gboolean motion_notify_event( GtkWidget * hWnd, GdkEventMotion * event );
+
 HB_FUNC( CREATELISTBOX )
 {
    GtkWidget * hViewPort = gtk_viewport_new( NULL, NULL );
@@ -34,6 +37,21 @@ HB_FUNC( CREATELISTBOX )
 
    gtk_signal_connect( GTK_OBJECT( hWnd ), "select-child",
                        GTK_SIGNAL_FUNC( LbxChangeEvent ), ( gpointer ) hScrollBars );
+
+   gtk_signal_connect( GTK_OBJECT( hScrollBars ), "button_press_event",
+                       ( GtkSignalFunc ) button_press_event, NULL );
+
+   gtk_signal_connect( GTK_OBJECT( hScrollBars ), "motion_notify_event",
+                       ( GtkSignalFunc ) motion_notify_event, NULL );
+
+   gtk_widget_set_events( hWnd, GDK_EXPOSURE_MASK
+			 | GDK_LEAVE_NOTIFY_MASK
+			 | GDK_BUTTON_PRESS_MASK
+			 | GDK_POINTER_MOTION_MASK
+			 | GDK_POINTER_MOTION_HINT_MASK );
+
+   gtk_widget_show( hViewPort );
+   gtk_widget_show( hWnd );
 
    hb_retnl( ( HB_ULONG ) hScrollBars );
 }
